d23ff22bbf0191357eecc95d07ad00e65685d388
vanhadson <vanhadson@gmail.com>
2023-02-26 16:30:00 +0300

    Step 2. Modified file "bin/gendiff.js" code so that when run with the "-h" flag, it outputs help with the additional option "-f (--format)"

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,7 +7,7 @@ program
     .name('gendiff')
     .description('Compares two configuration files and shows a difference.')
     .version('1.0.0', '-V, --version', 'output the version number')
-    .helpOption('-h, --help', 'output usage information')
-    .option('-f, --format <type>', 'output format');
+    .option('-f, --format <type>', 'output format')
+    .helpOption('-h, --help', 'output usage information');
 
 program.parse();
