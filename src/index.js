9eb57a5a9319c362b766ec3fb738b66a5534d7a3
vanhadson <vanhadson@gmail.com>
2023-03-05 13:45:59 +0300

    Step 3. Trying to get result of two files comparison (failure)

--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
-import { extname, resolve } from 'path';
+import { resolve } from 'path';
 import { readFileSync } from 'fs';
-import parsers from './parsers.js';
-import fileComparison from './fileComparison.js';
-import format from './formatters/index.js';
+//import parsers from './parsers.js';
+//import fileComparison from './fileComparison.js';
+//import format from './formatters/index.js';
 
-const getExtension = (filename) => extname(filename).slice(1);
-const getFixturePathToFile = (filename) => resolve(process.cwd(), filename);
-const readFile = (filename) => readFileSync(getFixturePathToFile(filename), 'utf-8');
+//const getExtension = (filename) => extname(filename).slice(1);
+const getFixturePathToFile1 = (filepath1) => resolve(process.cwd(), filepath1);
+const getFixturePathToFile2 = (filepath2) => resolve(process.cwd(), filepath2);
+//const readFile = (filename) => readFileSync(getFixturePathToFile(filename), 'utf-8');
 
-const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
-  // получение содержимого файлов в виде строки
-    const readFile1 = readFile(filepath1);
-    const readFile2 = readFile(filepath2);
-  // преобразование полученных строк в объекты с учетом формата файла (без точки)
-    const file1 = parsers(readFile1, getExtension(filepath1));
-    const file2 = parsers(readFile2, getExtension(filepath2));
-  // генерация массива объектов, описывающих различия в двух исходных объектах
-    const tree = fileComparison(file1, file2);
-  // Полученный массив объектов форматируем в нужного вида строку
-    return format(tree, formatName);
+const genDiff = (filepath1, filepath2) => {       
+
+  const file1 = JSON.parse(readFileSync(getFixturePathToFile1(filepath1), 'utf-8'));    
+
+  const file2 = JSON.parse(readFileSync(getFixturePathToFile2(filepath2), 'utf-8'));
+
+  const comparison = (file1, file2);
+
+  //const str = extname(comparison);
+
+  console.log(comparison);  
+
 };
 
 export default genDiff;
