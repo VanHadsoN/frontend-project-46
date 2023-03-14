ebbe1004c59237217214dfa7b66ea579a106cf4a
vanhadson <vanhadson@gmail.com>
2023-03-14 18:16:24 +0300

    Step 3. Created "src/parsers.js" (imported to "src/index.js")

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import path from 'path';
 import { readFileSync } from 'fs';
 import process from 'process';
+import parse from './parsers.js';
 import fileComparison from './fileComparison.js';
 import makeString from './makeString.js';
 
@@ -9,8 +10,8 @@ const getData = (filepath) => readFileSync(getAbsolutePath(filepath), 'utf-8');
 const getExtension = (filepath) => path.extname(filepath).slice(1);
 
 export default (filepath1, filepath2) => {
-  const obj1 = (getData(filepath1), getExtension(filepath1));
-  const obj2 = (getData(filepath2), getExtension(filepath2));
+  const obj1 = parse(getData(filepath1), getExtension(filepath1));
+  const obj2 = parse(getData(filepath2), getExtension(filepath2));
   const differences = fileComparison(obj1, obj2);
   return makeString(differences);
 };
