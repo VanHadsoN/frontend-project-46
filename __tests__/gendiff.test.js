406753090f33c4d450b7f1da4e1a0c9f4114c7e7
vanhadson <vanhadson@gmail.com>
2023-04-10 14:56:35 +0300

    Step 6. Modified "__tests__/gendiff.test.js" (for nested strucutures) and created "__fixtures__/stylishOutput.txt" (expected type of diff for nested structures)

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,26 +1,44 @@
-import { test, expect } from '@jest/globals';
-import path from 'path';
-import url from 'url';
+import { extname, dirname, resolve } from 'path';
 import { readFileSync } from 'fs';
-import genDiff from '../src/index.js';
+import { fileURLToPath } from 'url';
+import genTree from '../src/genTree.js';
+import format from '../src/formatters/index.js';
+import genDiff from '../index';
+import parsers from '../src/parsers.js';
 
-const __filename = url.fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const getFixturePath = (filename) => resolve(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
+const getExtension = (filename) => extname(filename).slice(1);
 
-const jsonFile1 = getFixturePath('file1.json');
-const jsonFile2 = getFixturePath('file2.json');
+const expectedStylishOutput = readFile('stylishOutput.txt');
+const expectedPlainOutput = readFile('plainOutput.txt');
+const expectedJsonOutput = readFile('jsonOutput.txt');
 
-const yamlFile1 = getFixturePath('file1.yaml');
-const yamlFile2 = getFixturePath('file2.yaml');
+const formatsFiles = ['json', 'yml', 'yaml'];
 
-const resultpath = getFixturePath('expected.txt');
-const result = readFileSync(resultpath, 'utf-8');
+test.each(formatsFiles)('different formats of files (.json, .yml, .yaml) & output styles', (formatFile) => {
+  const fileName1 = `file1.${formatFile}`;
+  const fileName2 = `file2.${formatFile}`;
+  const readFile1 = readFile(fileName1);
+  const readFile2 = readFile(fileName2);
+  const file1 = parsers(readFile1, getExtension(fileName1));
+  const file2 = parsers(readFile2, getExtension(fileName2));
 
-test.each([
-    { file1: jsonFile1, file2: jsonFile2, expected: result },
-    { file1: yamlFile1, file2: yamlFile2, expected: result },
-    ])('diff tests', (file1, file2, expected) => {
-        
-        expect(genDiff(file1, file2)).toBe(expected);
-    });
+  expect(format(genTree(file1, file2), 'stylish')).toEqual(expectedStylishOutput);
+  expect(format(genTree(file1, file2), 'plain')).toEqual(expectedPlainOutput);
+  expect(format(genTree(file1, file2), 'json')).toEqual(expectedJsonOutput);
+});
+
+test('additional parameter format is invalid test', () => {
+  const formatName = 'invalidParameter';
+  const file1 = parsers(readFile('file1.json'), 'json');
+  const file2 = parsers(readFile('file2.json'), 'json');
+
+  expect(() => format(genTree(file1, file2), formatName)).toThrow(`Unknown format to generate a tree: '${formatName}'!`);
+});
+
+test('wrong formats of files test', () => {
+  expect(() => genDiff('file1.txt', 'file2.txt')).toThrow();
+});
