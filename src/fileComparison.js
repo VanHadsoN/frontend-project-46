2014944d01f1ea4f894eb195a2b20d05e579f77a
vanhadson <vanhadson@gmail.com>
2023-04-13 13:24:56 +0300

    Step 6. Modified "src/fileComparison.js" (for nested structures)

--- src/fileComparison.js
+++ src/fileComparison.js
@@ -6,25 +6,50 @@ const fileComparison = (data1, data2) => {
   const allKeys = _.union(keys1, keys2);
   const sortedKeys = _.sortBy(allKeys);
 
-  const compareKeys = sortedKeys.map((key) => {
-    if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
-      return { key, value: fileComparison(data1[key], data2[key]), type: 'nested' };
-    } if (!_.has(data1, key)) {
-      return { key, value: data2[key], type: 'added' };
-    } if (!_.has(data2, key)) {
-      return { key, value: data1[key], type: 'deleted' };
-    } if (data1[key] !== data2[key]) {
+  const result = sortedKeys.map((key) => {
+    // Если текущего ключа НЕТ в файле1
+    if (!_.has(data1, key)) {
       return {
         key,
-        value1: data1[key],
-        value2: data2[key],
-        type: 'changed',
+        type: 'added',
+        value: data2[key],
       };
     }
-    return { key, value: data1[key], type: 'unchanged' };
+    // Если текущего ключа НЕТ в файле2
+    if (!_.has(data2, key)) {
+      return {
+        key,
+        type: 'removed',
+        value: data1[key],
+      };
+    }
+    // Если оба значения ключей являются объектами
+    // рекурсивно вызываем ф-цию fileComparison передавая ей значения ключей
+    if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
+      return {
+        key,
+        type: 'nested',
+        children: fileComparison(data1[key], data2[key]),
+      };
+    }
+    // Если значения текущего ключа НЕ равны друг другу (значениям в файле1 и файле2)
+    if (!_.isEqual(data1[key], data2[key])) {
+      return {
+        key,
+        type: 'updated',
+        removedValue: data1[key],
+        addedValue: data2[key],
+      };
+    }
+    // Если значение текущего ключа НЕ изменилось
+    return {
+      key,
+      type: 'unchanged',
+      value: data1[key],
+    };
   });
 
-  return compareKeys;
+  return result;
 };
 
 export default fileComparison;
