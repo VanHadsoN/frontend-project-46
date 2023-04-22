b505d595618e932dd9de21e77da6118a0e945249
vanhadson <vanhadson@gmail.com>
2023-04-22 12:55:30 +0300

    Fixing hexlet-check. Changed "bin/gendiff.js" (corrected spaces and added a line at end of file)

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,15 +5,15 @@ import genDiff from '../index.js';
 const program = new Command();
 
 program
-    .name('gendiff')
-    .usage('[options] <filepath1> <filepath2>')
-    .description('Compares two configuration files and shows a difference.')
-    .version('1.0.0', '-V, --version', 'output the version number')
-    .option('-f, --format <type>', 'output format', 'stylish')
-    .helpOption('-h, --help', 'output usage information')
-    .arguments('<filepath1> <filepath2>')
-    .action((filepath1, filepath2, options) => {
-        console.log(genDiff(filepath1, filepath2, options.format));
-    });
+  .name('gendiff')
+  .usage('[options] <filepath1> <filepath2>')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0', '-V, --version', 'output the version number')
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .helpOption('-h, --help', 'output usage information')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2, options) => {
+  console.log(genDiff(filepath1, filepath2, options.format));
+  });
 
 program.parse();
